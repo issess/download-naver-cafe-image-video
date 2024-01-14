67ca116e6511e6d3d9e90c53c02501de7f5aced8
SeungHoon Han <purebluecolor@gmail.com>
2024-01-15 03:49:55 +0900

    update image and video

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# download-naver-cafe-images
+# download-naver-cafe-images-and-videos
 
-네이버 카페에 있는 사진을 다운로드 하기 위해 만들었습니다.
+매주 어린이집에서 네이버 카페에 올리는 아이 사진을 하나하나 저장하기 힘들어 
+사진, 비디오, 내용, 코멘트를 한번에 다운로드 하기 위해 만들었습니다.
+정성스럽게 올려주시는 선생님께 감사드리며, 다운로드에 고통받는 분들이 줄어 들기를...  
+
+ * 다운로드 실패시 다시 시도기능. 
+ * 이미지,비디오 원본으로 다운로드
+ * Markdown으로 게시글 내용 및 코멘트 저장
 
 ## Setup
 
-파이선 가상환경 만들기
+가상환경 만들기
 
 ```
 $ python3 -m venv .venv
@@ -30,23 +36,43 @@ $ . .venv/bin/activate
 (.venv) $ deactivate
 ```
 
-## Usage
-
-도움말
-
+## Help
 ```commandline
 $ python main.py -h
+usage: main.py [-h] [-l] [-au ARTICLE_URL] [-lu LIST_URL]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -l, --login           login (default: False)
+  -au ARTICLE_URL, --article-url ARTICLE_URL
+                        download image in article (https://cafe.naver.com/ArticleRead.nhn....) (default: None)
+  -lu LIST_URL, --list-url LIST_URL
+                        download image in article list (https://cafe.naver.com/ArticleList.nhn...) (default: None)
 ```
 
-네이버 카페글에서 이미지 다운로드
+## Usage
+
+## 네이버 로그인
+로그인 정보를 저장에 체크하고 로그인을 최초 1회 합니다.
+
+```commandline
+$ python main.py -l
+```
+
+### 네이버 카페글에서 이미지 다운로드
+네이버 카페 게시물의 주소를 복사해서 아래와 같이 붙여 넣기 하시면 됩니다.
 
 ```commandline
 $ python main.py --article-url https://cafe.naver.com/ArticleRead.nhn...
 ```
 
-네이버 목록에서 각게시글 이미지 다운로드
+### 네이버 목록에서 각게시글 이미지 다운로드
+게시물 목록에서 게시글 url을 뽑아 리스트에 있는 데이터를 한번에 추출 할 수 있습니다.
+게시글 목록당 출력 갯수를 50개로 변경(카페 내에서)하여 주소를 입력하면 편하게 사용할 수 있습니다.
 
 ```commandline
 $ python main.py --article-url https://cafe.naver.com/ArticleList.nhn...
 ```
 
+## Result
+downloads 폴더에 게시글 이름으로 저장이 됩니다.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import shutil
 import sys
@@ -73,7 +74,7 @@ def file_exists(file_path):
 def createChromeDriver():
     chrome_options = webdriver.ChromeOptions()
     # head less
-    #chrome_options.add_argument('headless')
+    chrome_options.add_argument('headless')
     chrome_options.add_argument("--user-data-dir=selenium")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
@@ -83,16 +84,33 @@ def createChromeDriver():
     return driver
 
 
-def download_image_with_retry(img_url, title, save_filename, retry_count=0):
+def download_with_retry(img_url, directory, save_filename, retry_count=0):
     try:
-        # 이미지 다운로드
-        img_data = requests.get(img_url).content
+        file_name = f"{directory}/{save_filename}"
 
-        # 이미지 저장
-        with open(f"downloads/{title}/{save_filename}", 'wb') as f:
-            f.write(img_data)
+        # 파일 사이즈
+        resHead = requests.head(img_url)
+        total_length = resHead.headers.get('content-length')
+        if not file_exists(file_name) or total_length is None or os.path.getsize(file_name) != int(total_length):
+            with open(file_name, "wb") as f:
+                print("Downloading %s" % file_name)
+                response = requests.get(img_url, stream=True)
 
-        print(f"{save_filename} 저장 완료")
+                if total_length is None:  # no content length header
+                    f.write(response.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=4096):
+                        dl += len(data)
+                        f.write(data)
+                        done = int(50 * dl / total_length)
+                        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                        sys.stdout.flush()
+
+            print(f" {save_filename} 저장 완료. {total_length}")
+        else:
+            print(f" {save_filename} 이미 저장되었습니다.")
 
     except Exception as e:
         print(f"다운로드 실패: {e}")
@@ -101,7 +119,7 @@ def download_image_with_retry(img_url, title, save_filename, retry_count=0):
         if retry_count < 5:
             print(f"다시 시도합니다. 재시도 횟수: {retry_count + 1}")
             time.sleep(3)  # 재시도 간격
-            download_image_with_retry(img_url, title, save_filename, retry_count + 1)
+            download_with_retry(img_url, directory, save_filename, retry_count + 1)
         else:
             print("최대 재시도 횟수를 초과하였습니다.")
 
@@ -114,11 +132,11 @@ def login():
         time.sleep(1)
 
 
-def downloadImageByArticleByArticleUrl(url):
+def downloadContentsByArticleByArticleUrl(url):
     driver = createChromeDriver()
     try:
         driver.get(url)
-        iframe = WebDriverWait(driver, 5) \
+        iframe = WebDriverWait(driver, 3) \
             .until(EC.presence_of_element_located((By.CSS_SELECTOR, "#cafe_main")))
         # iframe으로 전환
         driver.switch_to.frame(iframe)
@@ -126,36 +144,28 @@ def downloadImageByArticleByArticleUrl(url):
         time.sleep(2)
         title = driver.find_element(by=By.CSS_SELECTOR,
                                     value="#app > div > div > div.ArticleContentBox > div.article_header > div.ArticleTitle > div > h3").text
-        article_date = driver.find_element(by=By.CSS_SELECTOR,
-                                           value="#app > div > div > div.ArticleContentBox > div.article_header > div.WriterInfo > div.profile_area > div.article_info > span.date").text
+        article_date = (driver.find_element(by=By.CSS_SELECTOR,
+                                            value="#app > div > div > div.ArticleContentBox > div.article_header > div.WriterInfo > div.profile_area > div.article_info > span.date")
+                        .text.replace(". ", "_").replace(".", "").replace(":", ""))
         description = driver.find_element(by=By.CSS_SELECTOR,
                                           value="#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div > div.content.CafeViewer > div > div").text
         comments = driver.find_element(by=By.CSS_SELECTOR,
                                        value="#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox").text
-        print()
-        print("url=" + title)
+        directory = f"downloads/{article_date}_{title}"
+        print("==============================================")
+        print("url=" + url)
         print("title=" + title)
         print("date=" + article_date)
         print("description=" + description)
         print("comments=" + comments)
-        create_directory(f"downloads/{title}")
-
-        images = []
-        try:
-            images = WebDriverWait(driver, 5) \
-                .until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[id^=\"SE-\"] > div > div > div > a > img")))
-        except:
-            print("Images not found")
-            pass
+        create_directory(directory)
 
         readmeHead = (f"{title}\n"
                       f"===================\n"
                       f"## 날짜\n"
                       f"{article_date}\n"
                       f"## 내용\n"
-                      f"```\n{description}\n```\n"
-                      f"## 사진\n")
+                      f"```\n{description}\n```\n")
 
         readmeFoot = (f"## 댓글\n"
                       f"```\n{comments}\n```")
@@ -163,27 +173,67 @@ def downloadImageByArticleByArticleUrl(url):
         readme = readmeHead
 
         # save photo
+        images = []
+        try:
+            images = WebDriverWait(driver, 5) \
+                .until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[id^=\"SE-\"] > div > div > div > a > img")))
+        except:
+            print("이미지가 없습니다.")
+            pass
+        if len(images) != 0:
+            readme += f"## 사진\n"
         for img in images:
             img_url = img.get_attribute('src').replace('?type=w1600', '')
             filename = extract_filename_from_url(img_url)
             filename_without_extension = remove_extension_from_filename(filename)
             save_filename = f"{filename_without_extension}_{images.index(img) + 1}.jpg"
-            readme += f"![alt text]({save_filename})\n"
+            readme += f"![{save_filename}]({save_filename})\n"
             # 이미지 다운로드 & 저장
-            # download_image_with_retry(img_url, title, save_filename)
-            # 이동
-            # move_file(save_filename,f"{title}/{save_filename}")
-            # validation
-            if not file_exists(f"downloads/{title}/{save_filename}"):
-                raise FileNotFoundError(f"파일 {save_filename}이 존재하지 않습니다.")
+            download_with_retry(img_url, directory, save_filename)
 
         print(f"이미지 {len(images)} 저장 완료")
 
+        # save video
+        videos = []
+        try:
+            videos = driver.find_elements(by=By.CSS_SELECTOR,
+                                          value="#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div > div.content.CafeViewer > div > div > div.se-component.se-video.se-l-default > script")
+            videos_names = driver.find_elements(by=By.CSS_SELECTOR,
+                                          value="#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div > div.content.CafeViewer > div > div > div.se-component.se-video.se-l-default > div > div > div.se-media-meta > div > strong > span")
+        except:
+            print("Videos not found")
+            pass
+        if len(videos) != 0:
+            readme += f"## 비디오\n"
+        for video in videos:
+            videoSourceJSON = json.loads(video.get_attribute("data-module"))
+            vid = videoSourceJSON['data']['vid']
+            inkey = videoSourceJSON['data']['inkey']
+            if vid and inkey:
+                videoJSON = json.loads(
+                    requests.get(f"https://apis.naver.com/rmcnmv/rmcnmv/vod/play/v2.0/{vid}?key={inkey}").text)
+                video_url = videoJSON['videos']['list'][-1]['source']
+                filename = extract_filename_from_url(video_url)
+                filename_without_extension = remove_extension_from_filename(filename)
+                #save_filename = f"{filename_without_extension}_{videos.index(video) + 1}.mp4"
+                new_save_filename = f"{videos_names[videos.index(video)].text}_{videos.index(video) + 1}.mp4"
+                readme += f"![{new_save_filename}]({new_save_filename})\n"
+                # 비디오 다운로드 & 저장
+                download_with_retry(video_url, directory, new_save_filename)
+                #move_file(f"{directory}/{save_filename}", f"{directory}/{new_save_filename}")
+
+
+            else:
+                print(f"vid={vid} key={key}정보가 없습니다.")
+        print(f"비디오 {len(videos)} 저장 완료")
+
         readme += readmeFoot
         # save README
-        save_string_to_file(f"downloads/{title}/README.md", readme)
+        save_string_to_file(f"{directory}/README.md", readme)
 
     except:
+        print("게시물 정보를 가져 오는데 실패했습니다.")
         traceback.print_exc(file=sys.stdout)
         pass
     finally:
@@ -192,6 +242,7 @@ def downloadImageByArticleByArticleUrl(url):
 
 def getArticleUrlListByList(url):
     try:
+        print(url)
         driver = createChromeDriver()
         driver.get(url)
         iframe = WebDriverWait(driver, 5) \
@@ -212,6 +263,7 @@ def getArticleUrlListByList(url):
             hrefs.append(href)
         return hrefs
     except:
+        print("게시물 리스트 정보를 가져 오는데 실패했습니다.")
         traceback.print_exc(file=sys.stdout)
         pass
     finally:
@@ -221,7 +273,6 @@ def getArticleUrlListByList(url):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-l", "--login", help="login", action='store_true')
-    parser.add_argument("--headless", help="headless", action='store_true')
     parser.add_argument("-au", "--article-url", type=str,
                         help="download image in article (https://cafe.naver.com/ArticleRead.nhn....) ")
     parser.add_argument("-lu", "--list-url", type=str,
@@ -230,10 +281,10 @@ if __name__ == "__main__":
     if args.login:
         login()
     elif args.article_url:
-        downloadImageByArticleByArticleUrl(args.article_url)
+        downloadContentsByArticleByArticleUrl(args.article_url)
     elif args.list_url:
         articles_url = getArticleUrlListByList(args.list_url)
-        for url in reversed(args.list):
-            downloadImageByArticleByArticleUrl(url)
+        for url in reversed(articles_url):
+            downloadContentsByArticleByArticleUrl(url)
     else:
         parser.print_usage()
